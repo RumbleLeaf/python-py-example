95a684bd2bc7a385a1d28e4161fde77990313db8
stephen <Sharpool316@gmail.com>
2018-07-02 18:57:12 -0500

    New Branch

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # python-py-example
 This is an example projuect to demonstrate the use of git on Ubuntu
 
-
+    //no longer need coments
 make a change here
--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def main(msg):
             //Print A Message
     print(msg)
     //Adding a different commit
+    //adding another comment
+def secondary():
+        print("New function")
 
 main("Hello People")
